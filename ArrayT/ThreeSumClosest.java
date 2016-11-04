f4f8982b33f1303a1e0a44e2ab4114a9aa1c339f
JeanJing <jeanjing0913@163.com>
2016-11-04 10:23:08 +0800

    代码还没有写完

---
+++ ArrayT/ThreeSumClosest.java
@@ -0,0 +1,16 @@
+package ArrayT;
+
+public class ThreeSumClosest {
+public int threeSumClosest(int[] nums, int target) {
+        int result = 0;
+        if(nums == null || nums.length == 0)
+        	return result;
+        int low, high;
+        for(int i = 0; i < nums.length - 2; i++){
+        	if( ){
+        		
+        	}
+        }
+        return result;
+    }
+}
