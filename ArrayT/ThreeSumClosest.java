054414f41161c649b447d56f054d3ba57a4577f5
JeanJing <jeanjing0913@163.com>
2016-11-04 11:25:22 +0800

    最初设置的closest的值是不对的，当你不知道怎么设时，可随意设置一个 符合条件的值，整体框架和3 sum相同

--- ArrayT/ThreeSumClosest.java
+++ ArrayT/ThreeSumClosest.java
@@ -1,16 +1,41 @@
 package ArrayT;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
-public int threeSumClosest(int[] nums, int target) {
-        int result = 0;
-        if(nums == null || nums.length == 0)
-        	return result;
-        int low, high;
-        for(int i = 0; i < nums.length - 2; i++){
-        	if( ){
-        		
-        	}
-        }
-        return result;
-    }
+	public static void main(String args[]) {
+		int[] data = { 0, 2, 1, -3 };
+		System.out.println(threeSumClosest(data, 1));
+	}
+
+	public static int threeSumClosest(int[] nums, int target) {
+
+		if (nums == null || nums.length < 3)
+			return 0;
+
+		Arrays.sort(nums);
+		int closest = nums[0] + nums[1] + nums[2];
+		int low, high;
+		for (int i = 0; i < nums.length - 2; i++) {
+			low = i + 1;
+			high = nums.length - 1;
+			
+			while (low < high) {
+				int close = nums[low] + nums[high] + nums[i];
+				if (close == target) {
+					return target;
+				} else {
+					if (Math.abs(close - target) < Math.abs(closest - target))
+						closest = close;
+					if (close < target) {
+						low++;
+
+					} else {
+						high--;
+					}
+				}
+			}
+		}
+		return closest;
+	}
 }
