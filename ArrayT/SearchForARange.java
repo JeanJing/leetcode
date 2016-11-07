dd4d2515bf34389875bf5ef0b023381395151193
JeanJing <jeanjing0913@163.com>
2016-11-07 16:41:32 +0800

    相比于第一种方法，第二种方法根据上下边界的特点分别用二分查找找到上下边界， 先找到上边界，再找下边界，

--- ArrayT/SearchForARange.java
+++ ArrayT/SearchForARange.java
@@ -2,7 +2,7 @@ package ArrayT;
 
 public class SearchForARange {
 	public static void main(String args[]){
-		int [] result = searchRange(new int[]{8},8);
+		int [] result = searchRange_binary(new int[]{ 8, 8},8);
 		for(int i  = 0; i < result.length; i++)
 			System.out.println(result[i]);
 	}
@@ -32,4 +32,63 @@ public class SearchForARange {
 		return  new int[]{-1, -1};
         
     }
+	public static int[] searchRange_binary(int[] nums, int target) {
+		if(nums == null || nums.length == 0)
+			return new int[]{-1, -1};
+		int low = 0;
+		int high = nums.length - 1;
+		int mid;
+		while(low <= high){
+			mid = low + (high - low) / 2;
+			if(target < nums[mid])
+				high = mid - 1;
+			else{
+				if(target > nums[mid])
+					low = mid + 1;
+				else{
+					if(mid > 0){
+						if(nums[mid - 1] < nums[mid]){
+							low = mid;
+							break;
+						}else{
+							high = mid - 1;
+						}
+						
+					}
+					else{
+						low = 0;
+						break;
+					}
+				}
+			}
+		}
+		int low2 = low;
+		high = nums.length -1;
+		while(low <= high){
+			mid = low2 + (high - low2) / 2;
+			if(target < nums[mid])
+				high = mid - 1;
+			else{
+				if(target > nums[mid])
+					low2 = mid + 1;
+				else{
+					if(mid < nums.length - 1){
+						if(nums[mid] < nums[mid+1]){
+							high = mid;
+							return new int[]{low, high};
+						}else{
+							low2 = mid + 1;
+						}
+						
+					}
+					else{
+						high = nums.length - 1;
+						return new int[]{low, high};
+						
+					}
+				}
+			}
+		}
+		return new int[]{-1,-1};
+	}
 }
